3a45a9a666a1f76c9eb7d6e6a738f81702f4403a
xCarlosDevx <lahozfrias@gmail.com>
2023-02-23 12:35:51 -0400

    inputs fixed

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -14,7 +14,6 @@ function App() {
             <Card key={index} data={item} index={index}></Card>
           ))}
         </div>
-
         <div className="inputs__Container">
           <div className="textContainer">
             <h3>Fill inputs</h3>
@@ -34,35 +33,41 @@ function App() {
             ></InputText>
           </div>
           <div className="buttonContainer">
-            <InputButton
-              label="Save"
-              fn={() =>
-                context.addTask({
-                  id:
-                    context.taskState.tasks.length == 0
-                      ? 1
-                      : context.taskState.tasks[
-                          context.taskState.tasks.length - 1
-                        ].id + 1,
-                  nameTask: context.nameState.taskName,
-                  descriptionTask: context.descriptionState.taskDescription,
-                  state: false,
-                })
-              }
-            ></InputButton>
-            <InputButton
-              label="Clear"
-              fn={() => {
-                context.nameState.setName("");
-                context.descriptionState.setDescription("");
-              }}
-            ></InputButton>
-            <InputButton
-              label="Send To Data Base"
-              fn={() => {
-                context.sendLS();
-              }}
-            />
+            <div className="button__one">
+              <InputButton
+                label="Save"
+                fn={() =>
+                  context.addTask({
+                    id:
+                      context.taskState.tasks.length == 0
+                        ? 1
+                        : context.taskState.tasks[
+                            context.taskState.tasks.length - 1
+                          ].id + 1,
+                    nameTask: context.nameState.taskName,
+                    descriptionTask: context.descriptionState.taskDescription,
+                    state: false,
+                  })
+                }
+              />
+            </div>
+            <div className="button__two">
+              <InputButton
+                label="Clear"
+                fn={() => {
+                  context.nameState.setName("");
+                  context.descriptionState.setDescription("");
+                }}
+              />
+            </div>
+            <div className="button__three">
+              <InputButton
+                label="Send To Data Base"
+                fn={() => {
+                  context.sendLS();
+                }}
+              />
+            </div>
           </div>
         </div>
       </div>
